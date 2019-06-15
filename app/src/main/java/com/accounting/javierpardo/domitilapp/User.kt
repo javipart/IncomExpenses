120eaaebe3174cbfdf200975d6a2626cfd7beb78
Javier <javipart@hotmail.com>
2019-06-15 09:53:04 -0500

    Initial commit

---
+++ app/src/main/java/com/accounting/javierpardo/domitilapp/User.kt
@@ -0,0 +1,16 @@
+package com.accounting.javierpardo.domitilapp
+
+class User
+{
+    var id : Int = 0
+    var password : String = ""
+
+    constructor(id: Int, password: String) {
+        this.id = id
+        this.password = password
+    }
+
+    constructor(){
+
+    }
+}
