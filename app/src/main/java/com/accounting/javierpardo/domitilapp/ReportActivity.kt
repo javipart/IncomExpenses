94908f09f44b0425dcbfc7ae8f98b3673ef0281b
Javier <javipart@hotmail.com>
2019-06-15 19:39:10 -0500

    Add titles to activities

--- app/src/main/java/com/accounting/javierpardo/domitilapp/ReportActivity.kt
+++ app/src/main/java/com/accounting/javierpardo/domitilapp/ReportActivity.kt
@@ -4,9 +4,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class ReportActivity : AppCompatActivity() {
+    private lateinit var title: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report)
+        this.setTitle(R.string.titleReport)
     }
 }
